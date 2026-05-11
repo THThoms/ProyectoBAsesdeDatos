8f14801c326632618109eae558769d54b57294b9
Tomas Solis <msolis5357@uta.edu.ec>
2026-05-10 19:15:49 -0500

    fix(backend): autoIncrement en PK de RESPONSABLE
    ID_RESP pasa a autoIncrement: true.

--- gitt/backend/src/models/Responsable.js
+++ gitt/backend/src/models/Responsable.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
 
 const Responsable = sequelize.define('RESPONSABLE', {
-  ID_RESP:  { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: false },
+  ID_RESP:  { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   NOM_RESP: { type: DataTypes.STRING(30), allowNull: false },
   APE_RESP: { type: DataTypes.STRING(30), allowNull: false },
   TIP_RESP: { type: DataTypes.STRING(20), allowNull: false },
